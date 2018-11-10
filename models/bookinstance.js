6782e27f9545c6994e8625ea006b002ad80226ac
Sami Kohvakka <sami.kohvakka@live.com>
2018-11-10 21:31:16 +0200

    Displaying library data

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-
+var moment = require('moment');
 var Schema = mongoose.Schema;
 
 var BookInstanceSchema = new Schema(
@@ -16,6 +16,13 @@ BookInstanceSchema
 .virtual('url')
 .get(function () {
   return '/catalog/bookinstance/' + this._id;
+});
+
+// Virtual for due date
+BookInstanceSchema
+.virtual('due_back_formatted')
+.get(function () {
+  return moment(this.due_back).format('MMMM Do, YYYY');
 });
 
 //Export model
